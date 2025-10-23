0dc1f7b34b10d4153758f9bb9008d0a089d373e9
saepudinasep <saepudinasep2001@gmail.com>
2025-10-23 10:00:58 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">
+        🚀 React + Tailwind + Vite
+      </h1>
+    </div>
+  );
+}
